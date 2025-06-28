3bce998530fb5f6258f44be206c0f3457fb35253
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-28 20:21:07 -0300

    Add root App.js for client

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function App() {
+  return (
+    <div style={{padding:20}}>
+      <h1>Group Rehearsal Scheduler</h1>
+      <p>Welcome to the rehearsal scheduler app!</p>
+      {/* More UI components coming soon */}
+    </div>
+  );
+}
+export default App;
